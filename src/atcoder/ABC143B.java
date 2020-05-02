d39732f699d2574126877be36b52b61f7020d8a8
ichi-nakashima <ichinaka3@gmail.com>
2020-05-02 11:11:25 +0900

    add ABC143 B&C

--- src/atcoder/ABC143B.java
+++ src/atcoder/ABC143B.java
@@ -2,22 +2,21 @@ package atcoder;
 
 import java.util.Scanner;
 
-public class Main {
+public class ABC143B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = Integer.parseInt(sc.next());
-        String S = sc.next();
-        String preS = "";
-        int r = 0;
-        for (char s:
-             S.toCharArray()) {
-            String curS = String.valueOf(s);
-            if (!preS.equals(curS)) {
-                preS = curS;
-                r++;
-            }
+        int[] numList = new int[N];
+        for (int i = 0; i < N; i++) {
+            numList[i] = Integer.parseInt(sc.next());
         }
 
+        int r = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                r += numList[i] * numList[j];
+            }
+        }
         System.out.println(r);
     }
 }
--- src/atcoder/ABC143C.java
+++ src/atcoder/ABC143C.java
@@ -2,22 +2,18 @@ package atcoder;
 
 import java.util.Scanner;
 
-public class Main {
+public class ABC143C {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = Integer.parseInt(sc.next());
-        String S = sc.next();
-        String preS = "";
+        char[] S = sc.next().toCharArray();
+        char preS = '.';
         int r = 0;
         for (char s:
-             S.toCharArray()) {
-            String curS = String.valueOf(s);
-            if (!preS.equals(curS)) {
-                preS = curS;
-                r++;
-            }
+             S) {
+            if (preS!=s) r++;
+            preS = s;
         }
-
         System.out.println(r);
     }
 }
