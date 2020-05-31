f25ac7aa338f0347d0f74bf33df5007731358243
ichi-nakashima <ichinaka3@gmail.com>
2020-05-31 10:36:45 +0900

    modify NOMURA2020 B

---
+++ src/atcoder/NOMURA2020BSandbox.java
@@ -0,0 +1,11 @@
+package atcoder;
+
+import java.util.Scanner;
+
+public class NOMURA2020BSandbox {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String T = sc.next();
+        System.out.println(T.replaceAll("\\?", "D"));
+    }
+}
